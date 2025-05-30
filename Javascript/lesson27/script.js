dec191d2dac30f0f22ccbe46da4821240a98f0e2
azamjonbro <azamjondasturchi11@gmail.com>
2025-05-30 23:42:50 +0500

    added

---
+++ Javascript/lesson27/script.js
@@ -0,0 +1,23 @@
+// let son = prompt("son kiriting")
+
+// juniorcha yozish
+// if(son>0){
+//  console.log("musbat");
+// }
+// else{
+//  console.log("manfiy");
+// }
+
+// yaxshi va optimal yozish
+
+// son >0?console.log("musbat"):console.log("manfiy");
+
+let a = 20
+let b = 20
+// confirm(a+b); 27-chi masala
+
+// 28-chi masala
+
+a ==b ?alert(0):alert(a+b);
+
+
